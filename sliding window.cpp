8527322ec7e1aa9b413f478b189618ba961b73e3
Vanu-2 <154403938+Vanu-2@users.noreply.github.com>
2024-10-24 00:39:56 +0600

    Add files via upload
    .

--- sliding window.cpp
+++ sliding window.cpp
@@ -1,107 +1,106 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-#define MAX_SEQ 1
 #define MAX_PKT 1024
-#define TOTAL_FRAMES 5
+#define TOTAL_FRAMES 3
 
-
-enum event_type{frame_arrival, timeout};
-typedef unsigned int seq_n;
+enum event_type{frame_arrival,timeout};
+typedef unsigned int s_n;
 
 struct packet{
-unsigned char data[MAX_PKT];
+    char data[MAX_PKT];
+
 };
 
 struct frame{
 
+s_n seq;
+s_n ack;
 packet info;
-seq_n ack;
-seq_n seq;
-
 };
-void inc(seq_n *num)
-{
-    *num=(*num+1)%(MAX_SEQ+1);
-}
-void from_network_layer(packet *p){
-cout<<"Fatch packet"<<endl;
 
+void wait_for_event(event_type &event)
+{
+    event=frame_arrival;
 }
 
-void to_network_layer(packet *p){
-cout<<"Deliver packet"<<endl;
-
-}
-
-void from_physical_layer(frame *r)
+void from_network(packet &p)
 {
-    r->seq=0;
-    r->ack=1;
+    cout<<"Fatching packet"<<endl;
 }
-
-void to_physical_layer(frame *s)
+void to_network(packet &p)
 {
-    cout<<"Transmit frame"<<endl;
+    cout<<"delivery packet"<<endl;
+}
+void to_physical(frame &s)
+{
+    cout<<"send frame of seq no:"<<s.seq<<" from receiving ack:"<<s.ack<<endl;
 }
 
-void wait_for_event(event_type *event)
+void from_physical(frame &r)
 {
-    *event=frame_arrival;
+    static s_n expected_frame=0;
+    r.seq=expected_frame;
+    r.ack=expected_frame;
+
+    cout<<"received frame of seq:"<<r.seq<<" replay ack :"<<r.ack<<endl;
+    expected_frame=(expected_frame+1)%2;
 }
-void start_timer(seq_n k)
+
+void start_timer(s_n seq)
 {
-    cout<<"start timer :"<<k<<endl;
+    cout<<"start timer for frame sequence:"<<seq<<endl;
 }
-void stop_timer(seq_n k)
+void stop_timer(s_n seq)
 {
-    cout<<"stop timer :"<<k<<endl;
+    cout<<"stop timer for frame sequence:"<<seq<<endl;
 }
+
 
 void protocol()
 {
-    seq_n next_frametosend=0;
-    seq_n expected_frame=0;
+
+    s_n next_frame=0;
+    s_n frame_expected=0;
     packet buffer;
     frame r,s;
     event_type event;
 
-    for(int i=0;i<TOTAL_FRAMES;i++)
-    {
-        from_network_layer(&buffer);
-        s.info=buffer;
-        s.seq=next_frametosend;
-        s.ack=1-expected_frame;
+    for(int i=0;i<TOTAL_FRAMES;i++){
 
-        cout<<"Send frame:"<<s.seq<<"  Ack:"<<s.ack<<endl;
-        to_physical_layer(&s);
-        start_timer(s.seq);
+       from_network(buffer);
+       s.info=buffer;
+       s.seq=next_frame;
+       s.ack=1-(frame_expected);
 
-        wait_for_event(&event);
+      to_physical(s);
+      start_timer(s.seq);
 
-        if(event==frame_arrival)
-        {
-            from_physical_layer(&r);
-            cout<<"received frame:"<<r.seq<<"Ack:"<<r.ack<<endl;
+      wait_for_event(event);
 
-            if(r.seq==expected_frame)
-            {
-                to_network_layer(&r.info);
-                inc(&expected_frame);
-            }
+      if(event==frame_arrival)
+      {
+          from_physical(r);
+          if(r.seq==frame_expected)
+          {
+              to_network(r.info);
+              frame_expected=(frame_expected+1)%2;
+          }
+          if(r.ack==next_frame)
+          {
+              stop_timer(next_frame);
+              next_frame=(next_frame+1)%2;
+          }
+      }
 
-            if(r.ack==next_frametosend)
-            {
-                stop_timer(r.ack);
-                inc(&next_frametosend);
-            }
-        }
     }
-    cout<<"All frame send and ack"<<endl;
+
+    cout<<"All frames are ack"<<endl;
+
+
 }
 
-int main() {
-    protocol();  // Start the 1-bit sliding window protocol
-    return 0;
+int main()
+{
+    protocol();
 }
